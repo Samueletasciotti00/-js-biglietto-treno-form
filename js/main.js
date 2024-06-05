e955f939b939d22941761fadc3bf6d7511fe4c24
Samuele <samueletasciotti@icloud.com>
2024-06-05 15:28:27 +0200

    Structure and program ok

--- js/main.js
+++ js/main.js
@@ -14,8 +14,16 @@ document.getElementById('conferma').addEventListener('click',
     const age = document.querySelector('select#seniority').value;
     console.log(age);
 
-   
 
+    //Stampaggio nome utente
+    document.getElementById('named').innerHTML = document.getElementById('utenteN').value;
+    
+    //Stampaggio Codice CP
+    document.getElementById('code').innerHTML = (Math.random() * 10000).toFixed(0);
+
+
+    //Stampaggio Carrozza
+    document.getElementById('carrozza').innerHTML = (Math.random() * 10).toFixed(0); 
 
     //Calcolo prezzo in base alla seniority
     if(age != 0){
@@ -27,11 +35,29 @@ document.getElementById('conferma').addEventListener('click',
         if(age === "minorenne"){
             priceN = (priceN * 0.8).toFixed(2);
             console.log(priceN);//Test Minorenne
+
+            //Stampaggio Offerta
+            document.getElementById('offer').innerHTML = 'Offerta Junior';
+
+            //Stampaggio Prezzo
+            document.getElementById('cost').innerHTML = priceN;
         } else if(age === "senior") {
             priceN = (priceN * 0.6).toFixed(2);
             console.log(priceN)
+
+            //Stampaggio Offerta
+            document.getElementById('offer').innerHTML = 'Offerta Senior';
+
+            //Stampaggio Prezzo
+            document.getElementById('cost').innerHTML = priceN;
         } else {
             console.log(priceN);
+
+            //Stampaggio Offerta
+            document.getElementById('offer').innerHTML = 'Biglietto Semplice';
+
+            //Stampaggio Prezzo
+            document.getElementById('cost').innerHTML = priceN;
         }
     } else {
         alert('Inserisci la tua età!!!')
